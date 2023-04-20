58c232a504e53e3d1e99fb4161de0c8effd0471e
Valeriy <valeriy.all@gmail.com>
2023-04-20 19:17:58 +0300

    fetchCountries

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -3,7 +3,11 @@ const countriesInfoFields = 'fields=name,capital,population,flags,languages';
 
 export function fetchCountries(name) {
   return fetch(`${countriesBase}${name}?${countriesInfoFields}`)
-    .then(response => response.json())
-    .catch(error => console.log(error));
+    .then(response => { 
+      if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })
+    // .catch(error => console.log(error));
 }
-
